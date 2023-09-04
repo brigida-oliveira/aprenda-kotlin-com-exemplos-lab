3b3e43c9b67f262b28b4ae76ad6c649f2e2ee4fc
Brígida Oliveira <ocfb0914@hotmail.com>
2023-09-04 15:02:38 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -4,7 +4,7 @@ enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
