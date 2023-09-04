93643e15539a11d8e03d0db37377f3d30b75cfbf
Brígida Oliveira <ocfb0914@hotmail.com>
2023-09-04 14:59:21 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,48 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("\nUsuário ${usuario.nome} matriculado/a na formação $nome.")
+    }
+    
+    fun listarInscritos() {
+        println("\nUsuários matriculados na formação $nome: ")
+        for (usuario in inscritos) {
+            println("* ${usuario.nome}")
+        }
+    }
+    
+    fun listarConteudosEducacionais() {
+        println("\nGrade Curricular de $nome: ")
+        for (conteudo in conteudos) {
+            println("* ${conteudo.nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val ana = Usuario("ana", "ana@gmail.com")
+    val pedro = Usuario("pedro", "pedro@gmail.com")
+    
+    val kotlin1 = ConteudoEducacional("Conhecendo o Kotlin e Sua Documentação Oficial", 60)
+    val kotlin2 = ConteudoEducacional("Introdução Prática à Linguagem de Programação Kotlin",120)
+    val kotlin3 = ConteudoEducacional("Estruturas de Controle de Fluxo e Coleções em Kotlin",120)
+    val kotlin4 = ConteudoEducacional("Orientação a Objetos e Tipos de Classes na Prática com Kotlin",120)
+    val kotlin5 = ConteudoEducacional("O Poder das Funções em Kotlin",120)
+    
+    val formacaoAndroid = Formacao("Formação Android Developer", listOf(kotlin1, kotlin2, kotlin3, kotlin4, kotlin5), Nivel.INTERMEDIARIO)
+    
+    formacaoAndroid.matricular(ana)
+    formacaoAndroid.matricular(pedro)
+    formacaoAndroid.listarInscritos()
+    formacaoAndroid.listarConteudosEducacionais()
 }
